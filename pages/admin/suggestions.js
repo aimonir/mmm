2bda1a77bd62ce9330b30caccd898f9f47a94146
Monirul Islam <hello@pythonhubs.com>
2025-08-30 03:33:24 +0600

    Fix: Correct placement of ESLint disable comment in admin/suggestions.js (final attempt)

--- pages/admin/suggestions.js
+++ pages/admin/suggestions.js
@@ -48,6 +48,7 @@ export default function ManageSuggestions() {
     
   
   
+  
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     const fetchSubjects = async () => {
